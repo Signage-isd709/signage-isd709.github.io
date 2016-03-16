31e151dc4c2568b7ac9c6d62df2c041996b29b41
Signage-isd709 <web.technology@isd709.org>
2016-03-16 17:45:03 -0500

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -8,7 +8,7 @@
         apiKey: 'AIzaSyCHhi-u9BPj-jFIqLVw4VKpzZUzeriG1ow',
         dateFormat: 'LongDate',
         errorMsg: 'No events in calendar',
-        maxEvents: 15,
+        maxEvents: 50,
         futureEventsOnly: true,
         sortDescending: true
       },

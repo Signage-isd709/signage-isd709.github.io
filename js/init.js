789c90d337605d9a20d24e4e965ff233e151998a
Signage-isd709 <web.technology@isd709.org>
2016-03-17 20:12:16 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,1 +1,1 @@
-head.load("http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js", "slick/slick.min.js", "js/calendar.js", "js/jquery.simpleWeather.js", "js/html2canvas.js", "js/stackblur.js", "css/style.css", "css/weatherStyle.css", "slick/slick.css");
+head.load("http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js", "slick/slick.min.js", "js/calendar.js", "js/jquery.simpleWeather.js", "css/style.css", "css/weatherStyle.css", "slick/slick.css");

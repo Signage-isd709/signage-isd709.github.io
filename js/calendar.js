822d046faf0e8568e3be4035e003c5aaecd4afa6
Signage-isd709 <web.technology@isd709.org>
2016-03-21 17:38:09 -0500

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -52,7 +52,7 @@
 					if(description) {
 						s +='<div class="description">'+ description +'</div>';
 					}
-					$($div).append('<div name="eventWrapper"><table><td><div name="eventIcon" align="left">' + calIcon + '</div></td><td><div name="eventInfo">' + s + '</div></td></table>');
+					$($div).append('<div name="eventWrapper" class="slide"><table><td><div name="eventIcon" align="left">' + calIcon + '</div></td><td><div name="eventInfo">' + s + '</div></td></table>');
         });
       },
       error: function(xhr, status) {
